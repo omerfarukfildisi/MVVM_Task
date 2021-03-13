5bd697aa5b90db54a92a5b7b53e76ba128d0dfe2
Ömer Faruk Fildişi <omerfarukfildisi@gmail.com>
2021-03-13 22:09:17 +0300

    Implemented the filtering feature

--- Launches/View/LaunchViewController.swift
+++ Launches/View/LaunchViewController.swift
@@ -15,7 +15,10 @@ class LaunchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     private var launchListViewModel: LaunchListViewModel!
     var launch: [Launch] = [Launch]()
+    var filteredLaunches: [Launch] = []
+    var launchCopy: [Launch] = []
     var pickerData: [String] = []
+    
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var yearPicker: UIPickerView!
@@ -27,27 +30,35 @@ class LaunchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         self.yearPicker.delegate = self
         self.yearPicker.dataSource = self
         
-        getData()
+        getData(isFiltered: false)
 
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.search, target: self, action: #selector(filterButtonClicked))
         
     }
     
-    private func getData() {
-              
-         let url = URL(string: "https://api.spacexdata.com/v2/launches")!
-         
-         Webservice().downloadLaunches(url: url) { launch in
-            if launch != nil {
-                self.launchListViewModel = LaunchListViewModel(launchList: launch!)
-                self.launch = launch!
-                self.pickerData = self.getYearsFromData(launch: launch!)
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                    self.yearPicker.reloadAllComponents()
+    private func getData(isFiltered: Bool) {
+        if(isFiltered){
+            self.launchListViewModel = LaunchListViewModel(launchList: filteredLaunches)
+            self.launch = filteredLaunches
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } else {
+            let url = URL(string: "https://api.spacexdata.com/v2/launches")!
+            
+            Webservice().downloadLaunches(url: url) { launch in
+               if launch != nil {
+                   self.launchListViewModel = LaunchListViewModel(launchList: launch!)
+                   self.launch = launch!
+                   self.launchCopy = launch!
+                   self.pickerData = self.getYearsFromData(launch: launch!)
+                   DispatchQueue.main.async {
+                       self.tableView.reloadData()
+                       self.yearPicker.reloadAllComponents()
+                   }
                 }
-             }
-         }
+            }
+        }
          
      }
     
@@ -56,7 +67,7 @@ class LaunchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func getYearsFromData(launch: [Launch])-> [String]{
-        var years: [String] = []
+        var years: [String] = ["All"]
         for l in launch {
             if (!years.contains(l.launch_year!)) {
                 years.append(l.launch_year!)
@@ -78,8 +89,15 @@ class LaunchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
 
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-            yearPicker.isHidden = true;
-        
+        if(row == 0){
+            self.filteredLaunches = launchCopy
+            self.getData(isFiltered: true)
+        }else{
+            let _launchCopy = launchCopy
+            self.filteredLaunches = _launchCopy.filter{$0.launch_year == pickerData[row]}
+            self.getData(isFiltered: true)
+        }
+        yearPicker.isHidden = true;
         }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.launchListViewModel == nil ? 0 : self.launchListViewModel.numberOfRowsInSection()
